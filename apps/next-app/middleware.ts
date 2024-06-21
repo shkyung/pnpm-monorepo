9bf48aefe2aacc4e37040ca88a4340e521fb07c3
sunny <sunny.kyung88@gmail.com>
2024-06-21 14:39:36 +0900

    feat: test

--- apps/next-app/middleware.ts
+++ apps/next-app/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.error('middleware called');
+  console.error("middleware called!!!1111");
   return NextResponse.redirect(new URL('/home', request.url));
 }
 
